875e469450f0d29489239b64856c31ba6d02e518
Ricardo FIgueroa <rfigueror@gmail.com>
2018-02-21 22:04:53 -0600

    crea proyecto

--- Polinomios/src/polinomial.adb
+++ Polinomios/src/polinomial.adb
@@ -4,11 +4,22 @@ use Ada.Text_IO;
 package body polinomial is
 
    function set_Polynomial(a: in Integer; b: in Integer) return coefficient is
-      x : coefficient;
+      x : coefficient := (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
       begin
       x(b) := a;
-    --deg : degree();
       return x;
    end set_Polynomial;
+
+   function set_degree(coef: in coefficient) return Integer is
+      d: Integer:= 0;
+   begin
+      for I in 1..20
+      loop
+         if coef(I)/=0 then
+            d := I;
+         end if;
+         end loop;
+      return d;
+   end set_degree;
 
  end polinomial;
