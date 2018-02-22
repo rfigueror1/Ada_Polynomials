94e75b305ce3141ea2051d7370eb087fa104084a
Ricardo FIgueroa <rfigueror@gmail.com>
2018-02-21 22:06:23 -0600

    crea proyecto

--- Polinomios/src/polinomial.ads
+++ Polinomios/src/polinomial.ads
@@ -1,9 +1,13 @@
 package polinomial is
 
-   type coefficient is array (1..15) of Integer;
-   function set_Polynomial(a: in Integer; b: in Integer) return coefficient;
-   --deg: Integer;
-   --type access_polynomials is access all Polynomial'Class;
-   --type Polynomial is record;
+   type coefficient is array (1..20) of Integer;
    
+   function set_Polynomial(a: in Integer; b: in Integer) return coefficient;
+   function set_degree(coef: in coefficient) return Integer;
+    
+   type Polynomial is record
+      degree: Integer;
+      coef: coefficient;
+   end record;
+      
 end polinomial;
